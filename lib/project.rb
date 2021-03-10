f0b41d30541a4c96f2bca6cc90a1d06d5ec855ac
Jesse Wenger <wengerj@gmail.com>
2021-03-10 04:19:52 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project 
+  attr_accessor :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def backers 
+    @backers
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+  end
+end
